42cd54da2021475f946035fe93fafa5d242bacf5
Akram Kassay <akram.kassay@gmail.com>
2018-04-15 15:55:25 -0700

    Plot stories seem to work

--- storybook/stories/gradient-bar.js
+++ storybook/stories/gradient-bar.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import { Defs, LinearGradient, Stop } from 'react-native-svg'
-import { LineChart } from 'react-native-svg-charts'
+import { LinearGradient, Stop, Defs } from 'react-native-svg'
+import { BarChart } from 'react-native-svg-charts'
 
-class GradientLineExample extends React.PureComponent {
+class GradientBarExample extends React.PureComponent {
 
     render() {
 
-        const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
+        const data    = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
 
         const Gradient = () => (
             <Defs key={ 'gradient' }>
-                <LinearGradient id={ 'gradient' } x1={ '0' } y={ '0%' } x2={ '100%' } y2={ '0%' }>
+                <LinearGradient id={ 'gradient' } x1={ '0%' } y={ '0%' } x2={ '0%' } y2={ '100%' }>
                     <Stop offset={ '0%' } stopColor={ 'rgb(134, 65, 244)' }/>
                     <Stop offset={ '100%' } stopColor={ 'rgb(66, 194, 244)' }/>
                 </LinearGradient>
@@ -18,20 +18,20 @@ class GradientLineExample extends React.PureComponent {
         )
 
         return (
-            <LineChart
+            <BarChart
                 style={{ height: 200 }}
                 data={ data }
                 contentInset={{ top: 20, bottom: 20 }}
                 svg={{
                     strokeWidth: 2,
-                    stroke: 'url(#gradient)',
+                    fill: 'url(#gradient)',
                 }}
             >
                 <Gradient/>
-            </LineChart>
+            </BarChart>
         )
     }
 
 }
 
-export default GradientLineExample
+export default GradientBarExample
