c4779beda6e6c87b848aa481fca8c8906277d4af
Akram Kassay <akram.kassay@gmail.com>
2018-04-15 18:29:57 -0700

    Super cool live plotting!

--- storybook/stories/accel-plotter.js
+++ storybook/stories/accel-plotter.js
@@ -3,6 +3,8 @@ import { DeviceEventEmitter, Text, View, TouchableHighlight } from 'react-native
 import { SensorManager } from 'NativeModules';
 import { Stopwatch } from 'react-native-stopwatch-timer';
 import ShowcaseCard from './decorators/showcase-container'
+import { Defs, LinearGradient, Stop } from 'react-native-svg'
+import { LineChart } from 'react-native-svg-charts'
 
 class AccelPlotter extends Component {
   constructor(props) {
@@ -34,7 +36,7 @@ class AccelPlotter extends Component {
   reset() {
     SensorManager.stopAccelerometer();
     this.data = []
-    this.setState({reset: true, running: false});
+    this.setState({data: this.data, reset: true, running: false});
   }
 
   toggle() {
@@ -50,8 +52,26 @@ class AccelPlotter extends Component {
   }
 
   render() {
+    const Gradient = () => (
+      <Defs key={ 'gradient' }>
+          <LinearGradient id={ 'gradient' } x1={ '0' } y={ '0%' } x2={ '100%' } y2={ '0%' }>
+              <Stop offset={ '0%' } stopColor={ 'rgb(134, 65, 244)' }/>
+              <Stop offset={ '100%' } stopColor={ 'rgb(66, 194, 244)' }/>
+          </LinearGradient>
+      </Defs>
+    )
+
     return (
       <View>
+        <ShowcaseCard>
+          <LineChart
+            style={{ height: 200 }}
+            data={ this.state.data }
+            contentInset={{ top: 20, bottom: 20 }}
+            svg={{strokeWidth: 2, stroke: 'url(#gradient)'}}>
+            <Gradient/>
+          </LineChart>
+        </ShowcaseCard>
         <ShowcaseCard>
           <View style={styles.stopwatch}>
             <Stopwatch msecs 
